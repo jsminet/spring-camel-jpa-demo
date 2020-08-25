40cdb09cbe4fa88e2b9b9702566106b6656afbc9
drioumo <driouchi@gmail.com>
2020-08-25 17:23:46 +0200

    Swaggre API

--- src/main/java/com/example/springcameljpademo/DemoApplication.java
+++ src/main/java/com/example/springcameljpademo/DemoApplication.java
@@ -1,13 +1,47 @@
 package com.example.springcameljpademo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@Configuration
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public Docket transactionApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.springcameljpademo.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(getApiInfo());
+	}
+
+	private ApiInfo getApiInfo() {
+		// Swagger API: http://localhost:8080/swagger-ui.html
+		return new ApiInfoBuilder()
+				.title("Transaction's API")
+				.version("1.0")
+				.description("Manage Transaction API")
+				.contact(new Contact("MD3431", "http://www.something.com", "email@something.com"))
+				.license("Spring Boot Version 2.3.0.RELEASE")
+				.build();
 	}
 }
